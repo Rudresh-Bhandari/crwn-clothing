3baea7fdce12fe7803778ae27e26863d6c37a24a
Rudresh-Bhandari <76592382+Rudresh-Bhandari@users.noreply.github.com>
2022-02-27 08:27:54 +0530

    Update stripe-button.component.jsx

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -3,7 +3,7 @@ import StripeCheckout from "react-stripe-checkout";
 
 const StripeCheckoutButton = ({price}) =>{
     const priceForStripe = price*100;
-    const publishableKey= 'pk_test_51KVp8HSFGXFr2xClMsoP9Msn4yqGhmcIiPBOmqwNDiHyXMDG9QhHy5K26fnuQqbYJt9Zs1tmKHZqX1IQ7GDPM7bO00qN49kIz8'
+    const publishableKey= // Add your own publishable key
 
 const onToken=token=>{
     console.log(token);
